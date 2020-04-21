74acbbf62d7e9040b465943816844754dabb4428
Mathias Ferniss <5211966@RTS-365.dk>
2020-04-21 14:32:42 +0200

    prøver at modulisering til at virke

--- src/app.js
+++ src/app.js
@@ -1,35 +1,35 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+  import React from 'react';
+  import {
+    render
+  } from 'react-dom'
+  import {
+    Pet
+  } from './Pet';
+  const App = () => {
+    return React.createElement(
+      "div", {
+        id: "div for et ide og det samme var sket hvis jeg skrev class"
+      },
+      [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+          name: "Luna",
+          animal: "Dog",
+          breed: "Havanese",
+        }),
 
-const App = () => {
-  return React.createElement(
-    "div",
-    { id: "div for et ide og det samme var sket hvis jeg skrev class" },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
+        React.createElement(Pet, {
+          name: "Pepper",
+          animal: "Bird",
+          breed: "Cockatiel",
+        }),
 
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel",
-      }),
-
-      React.createElement(Pet, {
-        name: "Doink",
-        animal: "Cat",
-        breed: "Mixed",
-      }),
-    ]
-  );
-};
-ReactDOM.render(React.createElement(App), document.querySelector("#root"));
+        React.createElement(Pet, {
+          name: "Doink",
+          animal: "Cat",
+          breed: "Mixed",
+        }),
+      ]
+    );
+  };
+  render(React.createElement(App), document.querySelector("#root"));
