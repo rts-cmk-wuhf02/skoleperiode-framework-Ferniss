8b6f55c169c79ca0f6367bae32e5808894dcf909
Mathias Ferniss <5211966@RTS-365.dk>
2020-04-23 11:21:01 +0200

    searhparams færdig

--- src/app.js
+++ src/app.js
@@ -1,40 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 const App = () => {
-  // return React.createElement(
-  //   "div", {
-  //     id: "div for et ide og det samme var sket hvis jeg skrev class"
-  //   },
-  //   [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, {
-  //       name: "Luna",
-  //       animal: "Dog",
-  //       breed: "Havanese",
-  //     }),
-
-  //     React.createElement(Pet, {
-  //       name: "Pepper",
-  //       animal: "Bird",
-  //       breed: "Cockatiel",
-  //     }),
-
-  //     React.createElement(Pet, {
-  //       name: "Doink",
-  //       animal: "Cat",
-  //       breed: "Mixed",
-  //     }),
-  //   ]
-  // );
-
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
+    // <div>
+    //   <h1 id="something-important">Adopt Me!</h1>
+    //   <Pet name="Luna" animal="Dog" breed="havanese" />
+    //   <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+    //   <Pet name="Doink" animal="Cat" breed="Mixed" />
+    // </div>
   );
 };
 render(<App />, document.querySelector("#root"));
