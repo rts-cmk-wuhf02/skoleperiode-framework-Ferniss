0a0b0a236c48ede2b64872402a62bb2ce99db399
Mathias Ferniss <5211966@RTS-365.dk>
2020-04-27 14:12:02 +0200

    skal have results til at virke

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from '@frontendmasters/pet';
+import Results from './Results';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -7,6 +8,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -17,10 +29,13 @@ const SearchParams = () => {
       setBreeds(breedStrings);
     }, console.error)
   }, [animal, setBreed, setBreeds]);
-  
+
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={e => {
+        e.preventDefault();
+        requestPets();
+      }}>
         <label htmlFor="location">
           location
           <input id="location" value={location} 
@@ -33,6 +48,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
